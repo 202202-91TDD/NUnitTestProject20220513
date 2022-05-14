7ce62111af0dbfadc891e9279f876f0c0da6ad1b
Joey Chen <jay.hatelove@gmail.com>
2022-05-14 21:49:41 +0800

    refactor: move Period class to independent file

--- NUnitTestProject20220513/BudgetService.cs
+++ NUnitTestProject20220513/BudgetService.cs
@@ -7,47 +7,6 @@ using System.Linq;
 
 namespace NUnitTestProject20220513
 {
-    public class Period
-    {
-        public Period(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
-
-        public DateTime End { get; private set; }
-        public DateTime Start { get; private set; }
-
-        public int OverlappingDays(Budget budget)
-        {
-            DateTime overlappingEnd;
-            DateTime overlappingStart;
-            if (Start.ToString("yyyyMM") == End.ToString("yyyyMM"))
-            {
-                overlappingEnd = End;
-                overlappingStart = Start;
-            }
-            else if (budget.YearMonth == Start.ToString("yyyyMM"))
-            {
-                overlappingEnd = budget.LastDay();
-                overlappingStart = Start;
-            }
-            else if (budget.YearMonth == End.ToString("yyyyMM"))
-            {
-                overlappingEnd = End;
-                overlappingStart = budget.FirstDay();
-            }
-            else
-            {
-                overlappingEnd = budget.LastDay();
-                overlappingStart = budget.FirstDay();
-            }
-
-            var overlappingDays = (overlappingEnd - overlappingStart).Days + 1;
-            return overlappingDays;
-        }
-    }
-
     public class BudgetService
     {
         private readonly IBudgetRepository budgetRepository;
