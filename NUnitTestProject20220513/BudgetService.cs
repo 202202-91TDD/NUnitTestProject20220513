3b7509ca605087cc9b0ee96a7617756f37dfac01
Joey Chen <jay.hatelove@gmail.com>
2022-05-14 19:29:51 +0800

    reformat

--- NUnitTestProject20220513/BudgetService.cs
+++ NUnitTestProject20220513/BudgetService.cs
@@ -1,19 +1,21 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+
+#endregion
 
 namespace NUnitTestProject20220513
 {
     public class BudgetService
     {
-        private IBudgetRepository budgetRepository;
+        private readonly IBudgetRepository budgetRepository;
 
         public BudgetService(IBudgetRepository _budgetRepository)
         {
             budgetRepository = _budgetRepository;
         }
-        
 
         public decimal Query(DateTime start, DateTime end)
         {
@@ -24,59 +26,57 @@ namespace NUnitTestProject20220513
 
             var data = budgetRepository.GetAll();
 
-                DateTime current = new DateTime(start.Year,start.Month, 1);
-                decimal total = 0;
-                while (true)
+            DateTime current = new DateTime(start.Year, start.Month, 1);
+            decimal total = 0;
+            while (true)
+            {
+                var budget = data.FirstOrDefault(m => m.YearMonth == current.ToString("yyyyMM")) ?? new Budget();
+
+                if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
                 {
-                    var budget = data.FirstOrDefault(m => m.YearMonth == current.ToString("yyyyMM")) ?? new Budget();
-
-                    if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
-                    {
-                        total +=  budget.Amount / DaysInMonth(current) * (end.Day - start.Day + 1);
-                    }
-                    else if (current.ToString("yyyyMM") == start.ToString("yyyyMM"))
-                    {
-                        total += budget.Amount / DaysInMonth(current) *
-                            (DaysInMonth(current) - start.Day + 1);
-                    }
-                    else if (current.ToString("yyyyMM") == end.ToString("yyyyMM"))
-                    {
-                        total +=  budget.Amount / DaysInMonth(current) * (end.Day);
-                    }
-                    else
-                    {
-                        total += budget.Amount;
-                    }
-
-                    if (current.ToString("yyyyMM") == end.ToString("yyyyMM"))
-                    {
-                        break;
-                    }
-
-                    current = current.AddMonths(1);
-
+                    total += budget.Amount / DaysInMonth(current) * (end.Day - start.Day + 1);
+                }
+                else if (current.ToString("yyyyMM") == start.ToString("yyyyMM"))
+                {
+                    total += budget.Amount /
+                        DaysInMonth(current) *
+                        (DaysInMonth(current) - start.Day + 1);
+                }
+                else if (current.ToString("yyyyMM") == end.ToString("yyyyMM"))
+                {
+                    total += budget.Amount / DaysInMonth(current) * (end.Day);
+                }
+                else
+                {
+                    total += budget.Amount;
                 }
 
-                return total;
+                if (current.ToString("yyyyMM") == end.ToString("yyyyMM"))
+                {
+                    break;
+                }
 
+                current = current.AddMonths(1);
+            }
 
-        }
-
-        private bool InvalidQueryDate(DateTime start, DateTime end)
-        {
-            return end < start;
+            return total;
         }
 
         private int DaysInMonth(DateTime start)
         {
             return DateTime.DaysInMonth(start.Year, start.Month);
         }
+
+        private bool InvalidQueryDate(DateTime start, DateTime end)
+        {
+            return end < start;
+        }
     }
 
     public class Budget
     {
-        public string YearMonth { get; set; }
         public int Amount { get; set; }
+        public string YearMonth { get; set; }
     }
 
     public interface IBudgetRepository
